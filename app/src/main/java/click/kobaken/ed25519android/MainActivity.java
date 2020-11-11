ca0b652dc940cb26e4307a77bd206f1a36a23e92
BharathKSunil <bharathk.sunil.k@gmail.com>
2020-11-11 12:27:02 +0530

    Update the Library to be used in Android Studio 4.1

--- app/src/main/java/click/kobaken/ed25519android/MainActivity.java
+++ app/src/main/java/click/kobaken/ed25519android/MainActivity.java
@@ -2,7 +2,7 @@ package click.kobaken.ed25519android;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
